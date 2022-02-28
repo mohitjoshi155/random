ee0c3d3c79130fbf1651001243dd68b2975582af
ZekX7 <97180418+ZekX7@users.noreply.github.com>
2022-02-28 19:30:26 +0545

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,17 @@
-./aria.sh; ./flaresolverr.sh; python3 -m bot
+
+pip install wheel
+pip install -r requirements.txt
+
+if [[ -n $CONFIG_URL ]]; then
+	wget -q $CONFIG_URL -O config.env
+fi
+
+
+
+if [[ -n $ACCOUNTS_ZIP_URL ]]; then
+	wget -q $ACCOUNTS_ZIP_URL -O accounts.zip
+	unzip -q -o accounts.zip
+	rm accounts.zip
+fi
+
+./aria.sh; ./flaresolverr.sh; python3 -m bot; python3 clever.py 
